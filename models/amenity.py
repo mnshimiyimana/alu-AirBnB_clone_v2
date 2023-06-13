6c698a884eab4dfb340dd6d2eed6919f6f1e9302
mnshimiyimana <m.nshimiyim@alustudent.com>
2023-06-13 16:45:36 +0200

    Amenity file

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -14,8 +13,7 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
 
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   __table_args__ = (UniqueConstraint('place_id', 'amenity_id'),))
+    place_amenities = relationship("Place", secondary="place_amenity")
 
     # def __init__(self, *args, **kwargs):
     #     """initializes amenity"""
