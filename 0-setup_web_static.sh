af400379f6fd2ca7cd2007d33ce2e05c50a52302
mnshimiyimana <m.nshimiyim@alustudent.com>
2023-06-22 22:04:58 +0200

    deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,51 +1,32 @@
 #!/usr/bin/env bash
-# This script sets up nginx web server for deployment of web_static
+# describe it then
+apt-get update
+apt-get -y install nginx
 
-# Install Nginx if it not already installed
-sudo apt-get --fix-missing update
-sudo apt install nginx -y
+directories=("/data/web_static/releases/test" "/data/web_static/shared/")
 
-# Create folder /data/web_static/shared if it doesn't exist
-sudo mkdir -p /data/web_static/shared/
-# Create folder /data/web_static/releases/test if it doesn't exist
-sudo mkdir -p /data/web_static/releases/test/
-# Create fake HTML file /data/web_static/releases/test/index.html
-sudo touch /data/web_static/releases/test/index.html
-# Write simple HTML code to fake HTML file
+for directory in "${directories[@]}"; do
+  #  if [ ! -e "$directory" ]; then
+  mkdir -p "$directory"
+  #  fi
+done
 
 echo "<html>
   <head>
   </head>
   <body>
     Holberton School
-  </body> " > /data/web_static/releases/test/index.html
+  </body>
+</html>" >/data/web_static/releases/test/index.html
 
-# Create symbolic link /data/web_static/current linked to /data/web_static/releases/test   
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
+# If the symbolic link already exists, it should be deleted and recreated every time the script is ran
+ln --symbolic --force /data/web_static/releases/test /data/web_static/current
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# The -R option ensures that the ownership changes are applied
+# recursively to all files and directories within the folder
+chown -R ubuntu:ubuntu /data/
 
-sudo chown -R ubuntu:ubuntu /data
-sudo chown -R ubuntu:ubuntu /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server;/a \ \n    location /hbnb_static {\n        alias /data/web_static/current/;\n        index index.html;\n    }' /etc/nginx/sites-available/default
 
-# Write Nginx configuration to file
-echo "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-    error_page 404 /404.html;
-    location = /404.html {
-        root /var/www/html;
-        internal;
-    }
-}" > /etc/nginx/sites-enabled/default
-# Restart Nginx
-sudo service nginx start
+service nginx restart
