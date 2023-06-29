0f64beb356ba95322d6c60c96bcf8ddcc235345c
mnshimiyimana <m.nshimiyim@alustudent.com>
2023-06-29 20:40:46 +0200

    Errors just

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
 """
-    python script that starts a Flask web application
+starts a web flask application
 """
 
-from models import storage
-from models.state import State
 from flask import Flask, render_template
+from models import storage
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states')
-def states_list():
-    """
-        Return: HTML page with list of states
-    """
-    path = '8-cities_by_states.html'
-    states = storage.all(State)
-    return render_template(path, states=states)
-
-
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """
-        Clean-up session
-    """
+def teardown_db(exception):
+    """Close storage"""
     storage.close()
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """List all cities by states"""
+    states = storage.all("State")
+    return render_template('8-cities_by_states.html', states=states)
+
+
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
